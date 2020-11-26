bc8d9df2753a78d71bd987c48539c1ddb02bd47a
itsme <itsme@xs4all.nl>
2020-11-26 15:02:53 +0100

    zipdump: fix seek bug

--- zipdump.py
+++ zipdump.py
@@ -7,6 +7,12 @@ Analyze PKZIP file contents
  * can operate on .zip files from a http://URL
 
 (C) 2016 Willem Hengeveld  <itsme@xs4all.nl>
+
+
+TODO:
+ - support stdin
+ - as library: add better interface, which allows enumeration of contents, lookup of files, extraction of files.
+
 """
 
 from __future__ import division, print_function, absolute_import, unicode_literals
@@ -17,6 +23,7 @@ import struct
 import datetime
 import zlib
 import itertools
+
 if sys.version_info[0] == 2:
     import scandir
     os.scandir = scandir.scandir
@@ -67,6 +74,7 @@ def zip_decrypt(data, pw):
             updatekeys(keys, b)
         yield u
 
+
 def skipbytes(blks, skip, args):
     """
     skip the first <skip> bytes of a stream of byte blocks.
@@ -86,20 +94,6 @@ def skipbytes(blks, skip, args):
         else:
             yield blk
 
-def decode_name(name):
-    nonprint = set('\u0009\u000b\u000c\u001c\u001d\u001e\u001f\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2008\u2009\u200a\u2028\u2029\u205f\u3000')
-    try:
-        utf8 = name.decode('utf-8', 'strict')
-        if not nonprint & set(utf8) and utf8.isprintable():
-            return utf8
-    except:
-        pass
-    return "hex-%s" % binascii.b2a_hex(name)
-
-def cvWinNTTime(nttime):
-    # convert 1601-01-01  .1usec epoch to  1970-01-01 1.0sec epoch
-    nttime /= 10000000
-    return nttime - 11644473600
 
 class EntryBase(object):
     """
@@ -122,24 +116,37 @@ class EntryBase(object):
     def loaditems(self, fh):
         """ loads any items refered to by the header """
         pass
+
     def summary(self):
         return ""
 
-def decodedatetime(ts):
-    def decode_date(dt):
-        if dt==0:
-            return datetime.datetime(1980,1,1)
-        year, mon, day = (dt>>9), (dt>>5)&15, dt&31
+    @staticmethod
+    def decode_name(name):
+        nonprint = set('\u0009\u000b\u000c\u001c\u001d\u001e\u001f\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2008\u2009\u200a\u2028\u2029\u205f\u3000')
         try:
-            return datetime.datetime(year+1980, mon, day)
-        except Exception as e:
-            print("error decoding date %d-%d-%d" % (year+1980, mon, day))
-            return datetime.datetime(1980,1,1)
-    def decode_time(tm):
-        hour, minute, bisecond = tm>>11, (tm>>5)&63, tm&31
-        return datetime.timedelta(hours=hour, minutes=minute, seconds=bisecond*2)
+            utf8 = name.decode('utf-8', 'strict')
+            if not nonprint & set(utf8) and utf8.isprintable():
+                return utf8
+        except:
+            pass
+        return "hex-%s" % binascii.b2a_hex(name)
 
-    return decode_date(ts>>16) + decode_time(ts & 0xFFFF)
+    @staticmethod
+    def decodedatetime(ts):
+        def decode_date(dt):
+            if dt==0:
+                return datetime.datetime(1980,1,1)
+            year, mon, day = (dt>>9), (dt>>5)&15, dt&31
+            try:
+                return datetime.datetime(year+1980, mon, day)
+            except Exception as e:
+                print("error decoding date %d-%d-%d" % (year+1980, mon, day))
+                return datetime.datetime(1980,1,1)
+        def decode_time(tm):
+            hour, minute, bisecond = tm>>11, (tm>>5)&63, tm&31
+            return datetime.timedelta(hours=hour, minutes=minute, seconds=bisecond*2)
+
+        return decode_date(ts>>16) + decode_time(ts & 0xFFFF)
 
 
 ######################################################
@@ -198,13 +205,15 @@ class FileEntryBase(EntryBase):
         if self.flags & ~testedflags:
             l.append("UNK_%x" % (self.flags & ~testedflags))
         return "+".join(l)
+
     def get_atime(self):
         return self.atime or self.get_mtime()
+
     def get_ctime(self):
         return self.ctime or self.get_mtime()
 
     def get_mtime(self):
-        return self.mtime or decodedatetime(self.timestamp).timestamp()
+        return self.mtime or self.decodedatetime(self.timestamp).timestamp()
 
     def updateTimes(self, mtime, atime, ctime):
         if atime:
@@ -235,6 +244,7 @@ class FileEntryBase(EntryBase):
             elif tag == 0x5855: # info-zip unix
                 self.parseInfoZip(self.extra[o:e])
             o = e
+
     def parseUnixUID(self, data):
         uid = gid = None
         o = 0
@@ -244,6 +254,7 @@ class FileEntryBase(EntryBase):
             print("WARNING: UnixUID: not enough data")
             return
         uid, gid = struct.unpack_from("<HH", data, 0)
+
     def parseUnixUIDNew(self, data):
         flag, = struct.unpack_from("<B", data, 0)
         uid = gid = None
@@ -272,6 +283,7 @@ class FileEntryBase(EntryBase):
                 print("WARNING: too many uid values")
             i += 1
         pass
+
     def parseTimestamp(self, data):
         atime = mtime = ctime = None
         flags, = struct.unpack_from("<B", data, 0)
@@ -301,6 +313,12 @@ class FileEntryBase(EntryBase):
         self.updateTimes(mtime, atime, ctime)
 
     def parseNtfs(self, data):
+
+        def cvWinNTTime(nttime):
+            # convert 1601-01-01  .1usec epoch to  1970-01-01 1.0sec epoch
+            nttime /= 10000000
+            return nttime - 11644473600
+
         atime = mtime = ctime = None
         o = 0
         while o < len(data):
@@ -313,6 +331,7 @@ class FileEntryBase(EntryBase):
                 # TODO: set m, a, c time
                 self.updateTimes(cvWinNTTime(mtime), cvWinNTTime(atime), cvWinNTTime(ctime))
             o += size
+
     def parseZip64(self, data):
         o = 0
         # zip64
@@ -329,10 +348,9 @@ class FileEntryBase(EntryBase):
 
     def getOriginalSize(self):
         return self.originalSize64 or rself.originalSize
+
     def getCompressedSize(self):
         return self.compressedSize64 or self.compressedSize
-
-
 
 
 class CentralDirEntry(FileEntryBase):
@@ -383,7 +401,7 @@ class CentralDirEntry(FileEntryBase):
 
     def loaditems(self, fh):
         fh.seek(self.nameOffset)
-        self.name = decode_name(fh.read(self.nameLength))
+        self.name = self.decode_name(fh.read(self.nameLength))
         fh.seek(self.extraOffset)
         self.extra = fh.read(self.extraLength)
         fh.seek(self.commentOffset)
@@ -394,7 +412,7 @@ class CentralDirEntry(FileEntryBase):
         return "%10d (%5.1f%%)  %s  %08x [%5s] %s" % (
                 self.originalSize,
                 100.0*self.compressedSize/self.originalSize if self.originalSize else 0,
-                decodedatetime(self.timestamp),
+                self.decodedatetime(self.timestamp),
                 self.crc32,
                 self.flagdesc(simple=True),
                 self.name
@@ -438,18 +456,17 @@ class CentralDirEntry(FileEntryBase):
 
     def havetimes(self):
         return True
+
     def havemode(self):
         return True
+
     def get_mode(self):
         return self.osAttrs >> 16
-
 
     def isdir(self):
         # for unix: upper 16 bits contain file perms.
         # lower 16 bits: 0 for regular, 16 for directory
         return (self.osAttrs & 65535) == 16
- 
-
 
 
 class LocalFileHeader(FileEntryBase):
@@ -495,7 +512,7 @@ class LocalFileHeader(FileEntryBase):
 
     def loaditems(self, fh):
         fh.seek(self.nameOffset)
-        self.name = decode_name(fh.read(self.nameLength))
+        self.name = self.decode_name(fh.read(self.nameLength))
         fh.seek(self.extraOffset)
         self.extra = fh.read(self.extraLength)
         # not loading data
@@ -504,7 +521,6 @@ class LocalFileHeader(FileEntryBase):
 
         if self.compressedSize == 0xFFFFFFFF:
             self.endOffset = self.dataOffset + self.getCompressedSize()
-
 
     def __repr__(self):
         r = "PK.0304: %04x %04x %04x %08x %08x %08x %08x %04x %04x |  %08x %08x %08x %s" % (
@@ -536,10 +552,13 @@ class LocalFileHeader(FileEntryBase):
 
     def havetimes(self):
         return (self.flags & 8) == 0
+
     def havemode(self):
         return False
+
     def get_mode(self):
         return None
+
     def isdir(self):
         # determine if this is an entry for a directory heuristically
         return self.crc32 == 0 and self.name.endswith('/')
@@ -690,7 +709,6 @@ class Zip64EndOfDir(EntryBase):
         r += ", %d byte directory" % self.dirSize
         return r
 
-
     def __repr__(self):
         return "PK.0606: %010x %04x %04x %4d/%4d %010x %010x %010x %010x | %010x %010x" % (
             self.hdrsize,
@@ -705,6 +723,7 @@ class Zip64EndOfDir(EntryBase):
             self.extensibleOffset,
             self.endOffset,
         )
+
     def reprPretty(self):
         r = "PK.0606: %s\n" % self.__class__.__name__
         r += "\tHeader size:                %08x\n" %  self.hdrsize
@@ -717,6 +736,7 @@ class Zip64EndOfDir(EntryBase):
         r += "\tcentral dir size:           %08x\n" %  self.dirSize
         r += "\tcentral dir offset:         %010x\n" % self.dirOffset
         return r
+
 
 class Zip64EndOfDirLocator(EntryBase):
     HeaderSize = 16
@@ -731,6 +751,7 @@ class Zip64EndOfDirLocator(EntryBase):
         ) = struct.unpack_from("<LQL", data, ofs)
         ofs += self.HeaderSize
         self.endOffset = baseofs + ofs
+
     def summary(self):
         return "LOC64 (%d of %d) -> 0x%010x" % (self.eodDiskId, self.nrOfDisks, self.eodOffset)
 
@@ -745,6 +766,7 @@ class Zip64EndOfDirLocator(EntryBase):
         r += "\tEOD64 offset:      %010x\n" % self.eodOffset
         r += "\tEOD64 total disks: %04x\n" % self.nrOfDisks
         return r
+
 
 class ArchiveExtraDataEntry(EntryBase):
     HeaderSize = 4
@@ -762,11 +784,13 @@ class ArchiveExtraDataEntry(EntryBase):
 
     def __repr__(self):
         return "PK.0806 %02x  | %08x" % (self.size, self.endOffset)
+
     def reprPretty(self):
         r = "PK.0806: %s\n" % self.__class__.__name__
         r += "\tsize  : %04x\n" % self.size
         r += "\tfield : %s\n" % binascii.b2a_hex(self.field)
         return r
+
 
 class SingleSpannedArchiveMarker(EntryBase):
     HeaderSize = 0
@@ -775,11 +799,14 @@ class SingleSpannedArchiveMarker(EntryBase):
     def __init__(self, baseofs, data, ofs):
         self.pkOffset = baseofs + ofs - 4
         self.endOffset = self.pkOffset + 4
+
     def __repr__(self):
         return "PK.0708"
+
     def reprPretty(self):
         r = "PK.0708: %s\n" % self.__class__.__name__
         return r
+
 
 class SpannedArchiveMarker(EntryBase):
     HeaderSize = 0
@@ -788,12 +815,13 @@ class SpannedArchiveMarker(EntryBase):
     def __init__(self, baseofs, data, ofs):
         self.pkOffset = baseofs + ofs - 4
         self.endOffset = self.pkOffset + 4
+
     def __repr__(self):
         return "PK.3030"
+
     def reprPretty(self):
         r = "PK.3030: %s\n" % self.__class__.__name__
         return r
-
 
 
 class ArchiveSignature(EntryBase):
@@ -807,8 +835,10 @@ class ArchiveSignature(EntryBase):
         self.signature = data[ofs:ofs+self.size]
         ofs += self.size
         self.endOffset = baseofs + ofs
+
     def __repr__(self):
         return "PK.0505 %02x  | %08x" % (self.size, self.endOffset)
+
     def reprPretty(self):
         r = "PK.0505: %s\n" % self.__class__.__name__
         r += "\tsize  : %04x\n" % self.size
@@ -821,6 +851,7 @@ def getDecoderClass(typ):
     for cls in (CentralDirEntry, LocalFileHeader, EndOfCentralDir, DataDescriptor, Zip64EndOfDir, Zip64EndOfDirLocator, ArchiveExtraDataEntry, ArchiveSignature, ):
         if cls.MagicNumber == typ:
             return cls
+
 
 def getMarkerClass(typ):
     """ Return Marker class for the PK type. """
@@ -898,7 +929,7 @@ def quickScanZip(args, fh):
 
     # read last 120 bytes of the file.
     ofs = max(filesize-120, 0)
-    fh.seek(ofs, os.SEEK_END)
+    fh.seek(ofs, os.SEEK_SET)
     eoddata = fh.read()
 
     iEND = eoddata.rfind(b'PK'+EndOfCentralDir.MagicNumber)
@@ -977,7 +1008,11 @@ def quickScanZip(args, fh):
         yield dirent
         dirofs = dirent.endOffset
 
+
 def zipraw(fh, ent):
+    """
+    yields the raw data blocks for a file entry.
+    """
     if isinstance(ent, CentralDirEntry):
         # find LocalFileHeader
         fh.seek(ent.dataOfs)
@@ -997,7 +1032,11 @@ def zipraw(fh, ent):
         yield block
         nread += len(block)
 
+
 def blockdump(baseofs, blks, limit):
+    """
+    Output a hexdump of the `blks` generator.
+    """
     o = baseofs
     if limit:
         e = baseofs + limit
@@ -1013,7 +1052,11 @@ def blockdump(baseofs, blks, limit):
         if e and o >= e:
             break
 
+
 def zipcat(blks, ent):
+    """
+    Generator which decompresses data from the `blks` generator.
+    """
     if ent.method==8:
         C = zlib.decompressobj(-15)
         for block in blks:
@@ -1043,7 +1086,11 @@ def namegenerator(name):
     for i in itertools.count(1):
         yield "%s-%d%s" % (part0, i, part1)
 
+
 def savefile(args, ent, name, data):
+    """
+    Saves all data from the `data` generator to the specified file.
+    """
     os.makedirs(os.path.dirname(os.path.join(args.outputdir, name)), exist_ok=True)
     for namei in namegenerator(name):
         path = os.path.join(args.outputdir, namei)
@@ -1060,7 +1107,8 @@ def savefile(args, ent, name, data):
 def getbytes(fh, ofs, size):
     fh.seek(ofs)
     return fh.read(size)
-    
+
+
 def processfile(args, fh):
     """ Process one opened file / url. """
     if args.quick:
@@ -1100,7 +1148,8 @@ def processfile(args, fh):
                     print("\n===> " + ent.name + " <===\n")
 
                 sys.stdout.flush()
-                blks = zipraw(fh, ent)
+
+                blks = zipraw(fh, ent)   # note that this is a generator.
 
                 if args.password and ent.flags&1:
                     blks = zip_decrypt(blks, args.password)
@@ -1130,6 +1179,7 @@ def processfile(args, fh):
                 print(ent.summary())
                 if hasattr(ent, "comment") and ent.comment and not args.dumpraw:
                     print(ent.comment)
+
             if args.dumpraw and hasattr(ent, "extraLength") and ent.extraLength:
                 print("%08x: XTRA: %s" % (ent.extraOffset, binascii.b2a_hex(getbytes(fh, ent.extraOffset, ent.extraLength))))
             if args.dumpraw and hasattr(ent, "comment") and ent.comment:
@@ -1267,6 +1317,7 @@ def main():
                     raise
     else:
         processfile(args, sys.stdin.buffer)
+
 
 if __name__ == '__main__':
     main()
